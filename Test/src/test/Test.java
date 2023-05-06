7124ee8d90b6a523d5d37753aebd2eaf560f5844
Evgeny Lukyanov <128099117+EvgenyLukyanov@users.noreply.github.com>
2023-05-06 23:55:41 +0300

    Update Test.java

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
     public String Cryptographer(String insert) {
         String output = "";
         char[] array = insert.toCharArray();
-        for (int i = 0; i < array.length - 1; i++) {
+        for (int i = 0; i < array.length; i++) {
             int ch = array[i];
             if (ch % 2 == 0) {
                 array[i] = (char) (ch * 2);
@@ -23,7 +23,7 @@ public class Test {
     public String DeCryptographer(String insert){
         String output="";
         char[] array = insert.toCharArray();
-        for (int i = 0; i < array.length - 1; i++) {
+        for (int i = 0; i < array.length; i++) {
             int ch = array[i];
             if (ch % 2 == 0) {
                 array[i] = (char) (ch / 2);
